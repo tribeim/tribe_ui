fc331e6b8d7249aca16e64c60798aa9fe17fa851
Sebastiaan Deckers <cbas@pandion.be>
2011-10-16 03:57:59 +0800

    Fixed code documentation generator

--- docs/generate.bat
+++ docs/generate.bat
@@ -1,4 +1,6 @@
 @cd ..
 @echo Generating NaturalDocs
+@if not exist "./docs/out" mkdir "./docs/out"
 @perl "/NaturalDocs/NaturalDocs" -i "./modules" -i "./core" -o HTML "./docs/out" -p "./docs"
+@echo Documentation available at "./docs/out/index.html"
 @pause
