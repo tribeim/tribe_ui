f082c49d51fc9b988a9061a6f83ca165e7a00e4e
Sebastiaan Deckers <cbas@pandion.be>
2011-10-29 19:04:50 +0800

    Unit test cases for core/promise (fixed the promise implementation, tdd ftw!)

--- core/promise.js
+++ core/promise.js
@@ -5,29 +5,29 @@ define(function () {
 			payload = undefined,
 			pending = 0,
 			promises = [],
-			unique = parseInt(Math.random() * 1000000),
+			execute = function () {
+				var _callbacks = callbacks;
+				callbacks = [];
+				_callbacks.forEach(function (callback) {
+					callback.apply(this, payload);
+				});
+			},
 			deferred = {
 				done: function () {
-					console.log("promise", unique, "done", pending);
-					isDone = true;
-					payload = arguments;
+					payload = arguments || payload;
 					pending--;
 					if (pending <= 0) {
-						console.log("promise", unique, "FEUER FREI!", pending);
-						callbacks.forEach(function (callback) {
-							callback.apply(this, payload);
-						});
+						isDone = true;
+						execute();
 					}
 					return this;
 				}
 			};
 		if (promisor instanceof Function) {
-			//setTimeout(function () { promisor(deferred); }, 0);
 			promisor(deferred);
 		}
 		return {
 			when: function (promise) {
-				//console.log("promise", unique, "when", pending);
 				if (promises.indexOf(promise) === -1) {
 					promises.push(promise);
 					pending++;
@@ -36,14 +36,10 @@ define(function () {
 				return this;
 			},
 			then: function (success /* , failure, progress */ ) { // TODO
-				//console.log("promise", unique, "then", pending);
 				if (callbacks.indexOf(success) === -1) {
 					callbacks.push(success);
 					if(isDone) {
-						console.log("promise", unique, "FEUER FREI! (THEN!!)", pending);
-						callbacks.forEach(function (callback) {
-							callback.apply(this, payload);
-						});
+						execute();
 					}
 				}
 				return this;
--- tests/core/promise.js
+++ tests/core/promise.js
@@ -1,4 +1,4 @@
-﻿define(function () {
+﻿define(["core/promise"], function (promise) {
 	module("promise", {
 		setup: function () {
 		},
@@ -6,8 +6,65 @@
 		}
 	});
 
-	test("Testing promises", 1, function () {
-		equal(true, true, "This cannot fail?");
+	test("Chaining", 2, function () {
+		var myPromise = new promise;
+		strictEqual(myPromise.when(new promise), myPromise, "When");
+		strictEqual(myPromise.then(new Function), myPromise, "Then");
+	});
+
+	test("Then callback", 2, function () {
+		(new promise(function(deferred) {
+			deferred.done();
+		}))
+		.then(function() {
+			ok(true, "Synchronous deferred fulfillment");
+		})
+		.then(function() {
+			ok(true, "Multiple then callbacks");
+		});
+	});
+
+	asyncTest("Asynchronous fulfillment", 1, function () {
+		(new promise(function(deferred) {
+			setTimeout(function () {
+				deferred.done();
+			}, 0);
+		}))
+		.then(function() {
+			ok(true, "Asynchronous deferred fulfillment");
+			start();
+		});
+	});
+
+	test("When condition", 1, function () {
+		var _deferred,
+			condition = new promise(function(deferred) {
+				_deferred = deferred;
+			});
+		(new promise())
+		.when(condition)
+		.then(function() {
+			ok(true, "Promise fulfilled by when condition");
+		});
+		_deferred.done();
+	});
+
+	test("Multiple when conditions", 1, function () {
+		var fulfilled = new promise(),
+			conditions = [];
+		[1, 2].forEach(function () {
+			fulfilled.when(
+				new promise(function (deferred) {
+					conditions.push(deferred);
+				})
+			);
+		}),
+		fulfilled.then(function () {
+			ok(true, "Promise fulfilled by when condition");
+		});
+		conditions.forEach(function (condition) {
+			condition.done();
+		});
 	});
 
 	return {};
